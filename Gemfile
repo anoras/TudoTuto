750521e237a3c928a65ec0fa9d19a425965e3892
Anders Norås <mail@andersnoras.com>
2012-04-24 15:56:21 +0200

    Empty project with dependencies.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'sqlite3'
+gem 'rails-backbone', '~> 0.7.0'
+gem 'jquery-rails'
+gem 'ejs'
+gem 'backbone-support'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoTuto::Application.config.secret_token = '871c7f7b0bd58c27836fd000cc55d3262b7d6b66d29fa9d152e3e79f482d6caaaa1519c310fe43398d11e08f2eaebac5b4f0fa47279fbb42ea88f6a68c937ccf'
